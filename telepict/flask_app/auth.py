24294f057da842a3db691c6c3256d6c53db21910
David Pitchford <david.t.pitchford@gmail.com>
2020-07-04 14:33:19 -0500

    Added edit account page to change display name and password

--- telepict/flask_app/auth.py
+++ telepict/flask_app/auth.py
@@ -7,8 +7,19 @@ from flask import Blueprint, render_template, request, current_app, flash, \
 from ..db import Player
 from ..auth import gen_password_hash
 from .exceptions import FlashedError
+from .util import inject_current_player
 
 bp = Blueprint('auth', __name__)
+
+def require_logged_in(func):
+    @functools.wraps(func)
+    def wrapped(*args, **kwargs):
+        if 'username' in flask_session:
+            return func(*args, **kwargs)
+
+        flash('You must login to view this page', 'warning')
+        return render_template('login.html')
+    return wrapped
 
 @bp.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,12 +63,22 @@ def create_account():
     flash(f'Account {name} created successfully!', 'primary')
     return redirect(url_for('auth.login'), 303)
 
-def require_logged_in(func):
-    @functools.wraps(func)
-    def wrapped(*args, **kwargs):
-        if 'username' in flask_session:
-            return func(*args, **kwargs)
+@bp.route('/edit_account', methods=['GET', 'POST'])
+@require_logged_in
+@inject_current_player
+def edit_account(session, player):
+    if request.method == 'GET':
+        return render_template('edit_account.html', player=player)
 
-        flash('You must login to view this page', 'warning')
-        return render_template('login.html')
-    return wrapped
+    dispname, new_password = request.form['dispname'], request.form['password']
+    changed = False
+    if dispname != player.display_name:
+        player.display_name = dispname
+        changed = True
+    if new_password:
+        player.password_hash = gen_password_hash(new_password, player.password_salt)
+        changed = True
+    if changed:
+        session.commit()
+        flash(f'Account changes applied', 'primary')
+    return redirect(url_for('auth.edit_account'), 303)
