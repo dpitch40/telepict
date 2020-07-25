636516c82cc8bbc53a7de217775ec37290afe592
David Pitchford <david.t.pitchford@gmail.com>
2020-07-24 21:48:22 -0500

    Added IDs to test data

--- scripts/init_db.py
+++ scripts/init_db.py
@@ -7,9 +7,9 @@ from telepict.config import Config
 
 def populate_db(d):
     # Players
-    david = Player(name='david', display_name='DarthMarth', password='12345678')
-    nathan = Player(name='Nathan', display_name='humcalc', password='you_shall_not_password')
-    elwood = Player(name='Elwood', display_name='Kalen', password='really good password')
+    david = Player(id_=1, name='david', display_name='DarthMarth', password='12345678')
+    nathan = Player(id_=2, name='Nathan', display_name='humcalc', password='you_shall_not_password')
+    elwood = Player(id_=3, name='Elwood', display_name='Kalen', password='really good password')
     
     # Games
     game = Game(id_=1, num_rounds=2, players=[david, nathan, elwood])
@@ -34,193 +34,193 @@ def populate_db(d):
     invitation3 = Invitation(recipient=elwood, game=pending_game_4)
 
     # Stacks
-    yesterday = Stack(game=game, owner=david)
-    hungry_cat = Stack(game=long_game, owner=david)
-    olympics = Stack(game=unfinished_game, owner=david)
-    enders_game = Stack(game=game, owner=nathan)
-    dediscovering = Stack(game=long_game, owner=nathan)
-    changing_stack = Stack(game=unfinished_game, owner=nathan)
-    eiffel_tower = Stack(game=game, owner=elwood)
-    fire_dice = Stack(game=unfinished_game, owner=elwood)
+    yesterday = Stack(id_=1, game=game, owner=david)
+    hungry_cat = Stack(id_=2, game=long_game, owner=david)
+    olympics = Stack(id_=3, game=unfinished_game, owner=david)
+    enders_game = Stack(id_=4, game=game, owner=nathan)
+    dediscovering = Stack(id_=5, game=long_game, owner=nathan)
+    changing_stack = Stack(id_=6, game=unfinished_game, owner=nathan)
+    eiffel_tower = Stack(id_=7, game=game, owner=elwood)
+    fire_dice = Stack(id_=8, game=unfinished_game, owner=elwood)
 
-    q_yesterday = Stack(game=queue_game, owner=david)
-    q_enders_game = Stack(game=queue_game, owner=nathan)
-    q_eiffel_tower = Stack(game=queue_game, owner=elwood)
+    q_yesterday = Stack(id_=9, game=queue_game, owner=david)
+    q_enders_game = Stack(id_=10, game=queue_game, owner=nathan)
+    q_eiffel_tower = Stack(id_=11, game=queue_game, owner=elwood)
 
-    d_stack_reverse = Stack(game=reverse_game, owner=david)
-    n_stack_reverse = Stack(game=reverse_game, owner=nathan)
-    e_stack_reverse = Stack(game=reverse_game, owner=elwood)
+    d_stack_reverse = Stack(id_=12, game=reverse_game, owner=david)
+    n_stack_reverse = Stack(id_=13, game=reverse_game, owner=nathan)
+    e_stack_reverse = Stack(id_=14, game=reverse_game, owner=elwood)
 
-    d_stack_drawfirst = Stack(game=draw_first_game, owner=david)
-    n_stack_drawfirst = Stack(game=draw_first_game, owner=nathan)
-    e_stack_drawfirst = Stack(game=draw_first_game, owner=elwood)
+    d_stack_drawfirst = Stack(id_=15, game=draw_first_game, owner=david)
+    n_stack_drawfirst = Stack(id_=16, game=draw_first_game, owner=nathan)
+    e_stack_drawfirst = Stack(id_=17, game=draw_first_game, owner=elwood)
 
-    pd_yesterday = Stack(game=partially_done_game, owner=david)
-    pd_enders_game = Stack(game=partially_done_game, owner=nathan)
-    pd_eiffel_tower = Stack(game=partially_done_game, owner=elwood)
+    pd_yesterday = Stack(id_=18, game=partially_done_game, owner=david)
+    pd_enders_game = Stack(id_=19, game=partially_done_game, owner=nathan)
+    pd_eiffel_tower = Stack(id_=20, game=partially_done_game, owner=elwood)
 
     # Game
-    dw_11 = Writing(text="Coming soon: yesterday",
+    dw_11 = Writing(id_=1, text="Coming soon: yesterday",
                     stack_pos=0, stack=yesterday, author=david)
-    dw_12 = Writing(text="Be kind, bake your VHS tapes at 400 degrees for "
+    dw_12 = Writing(id_=2, text="Be kind, bake your VHS tapes at 400 degrees for "
                          "20 minutes before returning",
                     stack_pos=2, stack=enders_game, author=david)
-    dw_13 = Writing(text="The pyramid is sad because it fell over. "
+    dw_13 = Writing(id_=3, text="The pyramid is sad because it fell over. "
                          "People measure it instead of helping.",
                     stack_pos=4, stack=eiffel_tower, author=david)
 
-    nw_11 = Writing(text="Ender's Game",
+    nw_11 = Writing(id_=4, text="Ender's Game",
                     stack_pos=0, stack=enders_game, author=nathan)
-    nw_12 = Writing(text="The Eiffel Tower is sad that it's being subjected to a linear "
+    nw_12 = Writing(id_=5, text="The Eiffel Tower is sad that it's being subjected to a linear "
                          "transformation that's stretching in the x direction and "
                          "compressing in the y direction.",
                     stack_pos=2, stack=eiffel_tower, author=nathan)
-    nw_13 = Writing(text="Daylight Savings starts on Monday the 21st this year "
+    nw_13 = Writing(id_=6, text="Daylight Savings starts on Monday the 21st this year "
                          "for some reason.",
                     stack_pos=4, stack=yesterday, author=nathan)
 
-    ew_11 = Writing(text="An Awful Eiffel Tower",
+    ew_11 = Writing(id_=7, text="An Awful Eiffel Tower",
                     stack_pos=0, stack=eiffel_tower, author=elwood)
-    ew_12 = Writing(text="Passing an hour on the fourth Monday.",
+    ew_12 = Writing(id_=8, text="Passing an hour on the fourth Monday.",
                     stack_pos=2, stack=yesterday, author=elwood)
-    ew_13 = Writing(text="Roomba is ready to vacuum up the shocked TV person's mess",
+    ew_13 = Writing(id_=9, text="Roomba is ready to vacuum up the shocked TV person's mess",
                     stack_pos=4, stack=enders_game, author=elwood)
 
-    dd_11 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_11.jpg'), 'rb').read(),
+    dd_11 = Drawing(id_=1, drawing=open(os.path.join('data', 'drawings', 'dd_11.jpg'), 'rb').read(),
                     stack_pos=1, stack=eiffel_tower, author=david)
-    dd_12 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_12.jpg'), 'rb').read(),
+    dd_12 = Drawing(id_=2, drawing=open(os.path.join('data', 'drawings', 'dd_12.jpg'), 'rb').read(),
                     stack_pos=3, stack=yesterday, author=david)
-    dd_13 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_13.jpg'), 'rb').read(),
+    dd_13 = Drawing(id_=3, drawing=open(os.path.join('data', 'drawings', 'dd_13.jpg'), 'rb').read(),
                     stack_pos=5, stack=enders_game, author=david)
 
-    nd_11 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
+    nd_11 = Drawing(id_=4, drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
                     stack_pos=1, stack=yesterday, author=nathan)
-    nd_12 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_12.jpg'), 'rb').read(),
+    nd_12 = Drawing(id_=5, drawing=open(os.path.join('data', 'drawings', 'nd_12.jpg'), 'rb').read(),
                     stack_pos=3, stack=enders_game, author=nathan)
-    nd_13 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_13.jpg'), 'rb').read(),
+    nd_13 = Drawing(id_=6, drawing=open(os.path.join('data', 'drawings', 'nd_13.jpg'), 'rb').read(),
                     stack_pos=5, stack=eiffel_tower, author=nathan)
 
-    ed_11 = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_11.jpg'), 'rb').read(),
+    ed_11 = Drawing(id_=7, drawing=open(os.path.join('data', 'drawings', 'ed_11.jpg'), 'rb').read(),
                     stack_pos=1, stack=enders_game, author=elwood)
-    ed_12 = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_12.jpg'), 'rb').read(),
+    ed_12 = Drawing(id_=8, drawing=open(os.path.join('data', 'drawings', 'ed_12.jpg'), 'rb').read(),
                     stack_pos=3, stack=eiffel_tower, author=elwood)
-    ed_13 = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_13.jpg'), 'rb').read(),
+    ed_13 = Drawing(id_=9, drawing=open(os.path.join('data', 'drawings', 'ed_13.jpg'), 'rb').read(),
                     stack_pos=5, stack=yesterday, author=elwood)
 
     # Long game
-    dw_21 = Writing(text="A cat (hungry) on a conveyor belt to Mexican Hell. "
+    dw_21 = Writing(id_=10, text="A cat (hungry) on a conveyor belt to Mexican Hell. "
                          "Old mice watch.",
                     stack_pos=0, stack=hungry_cat, author=david)
-    dw_22 = Writing(text="Europeans waiting in line for food and communism",
+    dw_22 = Writing(id_=11, text="Europeans waiting in line for food and communism",
                     stack_pos=2, stack=hungry_cat, author=david)
-    dw_23 = Writing(text="Daggers piercing balloons makes for a sunny week.",
+    dw_23 = Writing(id_=12, text="Daggers piercing balloons makes for a sunny week.",
                     stack_pos=4, stack=hungry_cat, author=david)
-    dw_24 = Writing(text="Recursive sunset chainsaw massacre",
+    dw_24 = Writing(id_=13, text="Recursive sunset chainsaw massacre",
                     stack_pos=6, stack=hungry_cat, author=david)
 
-    nw_21 = Writing(text="The de-discovering of America. The Europeans were afraid of the colossi.",
+    nw_21 = Writing(id_=14, text="The de-discovering of America. The Europeans were afraid of the colossi.",
                     stack_pos=0, stack=dediscovering, author=nathan)
-    nw_22 = Writing(text="Elworld. The Elwood,themed theme park",
+    nw_22 = Writing(id_=15, text="Elworld. The Elwood,themed theme park",
                     stack_pos=2, stack=dediscovering, author=nathan)
-    nw_23 = Writing(text="A dinosaur texting his prey.",
+    nw_23 = Writing(id_=16, text="A dinosaur texting his prey.",
                     stack_pos=4, stack=dediscovering, author=nathan)
-    nw_24 = Writing(text="Dead puppies make new puppies",
+    nw_24 = Writing(id_=17, text="Dead puppies make new puppies",
                     stack_pos=6, stack=dediscovering, author=nathan)
 
-    dd_21 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_21.jpg'), 'rb').read(),
+    dd_21 = Drawing(id_=10, drawing=open(os.path.join('data', 'drawings', 'dd_21.jpg'), 'rb').read(),
                     stack_pos=1, stack=dediscovering, author=david)
-    dd_22 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_22.jpg'), 'rb').read(),
+    dd_22 = Drawing(id_=11, drawing=open(os.path.join('data', 'drawings', 'dd_22.jpg'), 'rb').read(),
                     stack_pos=3, stack=dediscovering, author=david)
-    dd_23 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_23.jpg'), 'rb').read(),
+    dd_23 = Drawing(id_=12, drawing=open(os.path.join('data', 'drawings', 'dd_23.jpg'), 'rb').read(),
                     stack_pos=5, stack=dediscovering, author=david)
-    dd_24 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_24.jpg'), 'rb').read(),
+    dd_24 = Drawing(id_=13, drawing=open(os.path.join('data', 'drawings', 'dd_24.jpg'), 'rb').read(),
                     stack_pos=7, stack=dediscovering, author=david)
 
-    nd_21 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_21.jpg'), 'rb').read(),
+    nd_21 = Drawing(id_=14, drawing=open(os.path.join('data', 'drawings', 'nd_21.jpg'), 'rb').read(),
                     stack_pos=1, stack=hungry_cat, author=nathan)
-    nd_22 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_22.jpg'), 'rb').read(),
+    nd_22 = Drawing(id_=15, drawing=open(os.path.join('data', 'drawings', 'nd_22.jpg'), 'rb').read(),
                     stack_pos=3, stack=hungry_cat, author=nathan)
-    nd_23 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_23.jpg'), 'rb').read(),
+    nd_23 = Drawing(id_=16, drawing=open(os.path.join('data', 'drawings', 'nd_23.jpg'), 'rb').read(),
                     stack_pos=5, stack=hungry_cat, author=nathan)
-    nd_24 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_24.jpg'), 'rb').read(),
+    nd_24 = Drawing(id_=17, drawing=open(os.path.join('data', 'drawings', 'nd_24.jpg'), 'rb').read(),
                     stack_pos=7, stack=hungry_cat, author=nathan)
 
     # Unfinished game
-    dw_31 = Writing(text="The Olympic hide-and-seek championships",
+    dw_31 = Writing(id_=18, text="The Olympic hide-and-seek championships",
                     stack_pos=0, stack=olympics, author=david)
-    dw_32 = Writing(text="1d4, 1d6, 1d8 roasting on an open fire",
+    dw_32 = Writing(id_=19, text="1d4, 1d6, 1d8 roasting on an open fire",
                     stack_pos=2, stack=fire_dice, author=david)
 
-    nw_31 = Writing(text="Changing the meaning of the stack in Telephone Pictionary",
+    nw_31 = Writing(id_=20, text="Changing the meaning of the stack in Telephone Pictionary",
                     stack_pos=0, stack=changing_stack, author=nathan)
-    nw_32 = Writing(text="Olympic fan waving",
+    nw_32 = Writing(id_=21, text="Olympic fan waving",
                     stack_pos=2, stack=olympics, author=nathan)
 
-    ew_31 = Writing(text="Fire Dice",
+    ew_31 = Writing(id_=22, text="Fire Dice",
                     stack_pos=0, stack=fire_dice, author=elwood)
-    ew_32 = Writing(text="Nerd is angry/surprised that things are collating the "
+    ew_32 = Writing(id_=23, text="Nerd is angry/surprised that things are collating the "
                          "wrong way when printed",
                     stack_pos=2, stack=changing_stack, author=elwood)
 
-    dd_31 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_31.jpg'), 'rb').read(),
+    dd_31 = Drawing(id_=18, drawing=open(os.path.join('data', 'drawings', 'dd_31.jpg'), 'rb').read(),
                     stack_pos=1, stack=changing_stack, author=david)
-    dd_32 = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_32.jpg'), 'rb').read(),
+    dd_32 = Drawing(id_=19, drawing=open(os.path.join('data', 'drawings', 'dd_32.jpg'), 'rb').read(),
                     stack_pos=3, stack=olympics, author=david)
 
-    nd_31 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_31.jpg'), 'rb').read(),
+    nd_31 = Drawing(id_=20, drawing=open(os.path.join('data', 'drawings', 'nd_31.jpg'), 'rb').read(),
                     stack_pos=1, stack=fire_dice, author=nathan)
-    nd_32 = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_32.jpg'), 'rb').read(),
+    nd_32 = Drawing(id_=21, drawing=open(os.path.join('data', 'drawings', 'nd_32.jpg'), 'rb').read(),
                     stack_pos=3, stack=changing_stack, author=nathan)
 
-    ed_31 = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_31.jpg'), 'rb').read(),
+    ed_31 = Drawing(id_=22, drawing=open(os.path.join('data', 'drawings', 'ed_31.jpg'), 'rb').read(),
                     stack_pos=1, stack=olympics, author=elwood)
-    ed_32 = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_32.jpg'), 'rb').read(),
+    ed_32 = Drawing(id_=23, drawing=open(os.path.join('data', 'drawings', 'ed_32.jpg'), 'rb').read(),
                     stack_pos=3, stack=fire_dice, author=elwood)
 
     # Queue game
-    q_dw = Writing(text="Coming soon: yesterday",
+    q_dw = Writing(id_=24, text="Coming soon: yesterday",
                    stack_pos=0, stack=q_yesterday, author=david)
-    q_nw = Writing(text="Ender's Game",
+    q_nw = Writing(id_=25, text="Ender's Game",
                    stack_pos=0, stack=q_enders_game, author=nathan)
-    q_ew = Writing(text="An Awful Eiffel Tower",
+    q_ew = Writing(id_=26, text="An Awful Eiffel Tower",
                    stack_pos=0, stack=q_eiffel_tower, author=elwood)
-    q_nd = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
+    q_nd = Drawing(id_=24, drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
                    stack_pos=1, stack=q_yesterday, author=nathan)
 
     # Reverse game
-    r_dw = Writing(text="Europeans waiting in line for food and communism",
+    r_dw = Writing(id_=27, text="Europeans waiting in line for food and communism",
                    stack_pos=0, stack=d_stack_reverse, author=david)
-    r_nw = Writing(text="Elworld. The Elwood,themed theme park",
+    r_nw = Writing(id_=28, text="Elworld. The Elwood,themed theme park",
                    stack_pos=0, stack=n_stack_reverse, author=nathan)
-    r_ew = Writing(text="A dinosaur texting his prey.",
+    r_ew = Writing(id_=29, text="A dinosaur texting his prey.",
                    stack_pos=0, stack=e_stack_reverse, author=elwood)
 
     # Draw first game
-    df_ed = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_21.jpg'), 'rb').read(),
+    df_ed = Drawing(id_=25, drawing=open(os.path.join('data', 'drawings', 'nd_21.jpg'), 'rb').read(),
                     stack_pos=0, stack=e_stack_drawfirst, author=elwood)
 
     # Partially done game
-    dw_11_pd = Writing(text="Coming soon: yesterday",
+    dw_11_pd = Writing(id_=30, text="Coming soon: yesterday",
                        stack_pos=0, stack=pd_yesterday, author=david)
-    dw_12_pd = Writing(text="Be kind, bake your VHS tapes at 400 degrees for "
+    dw_12_pd = Writing(id_=31, text="Be kind, bake your VHS tapes at 400 degrees for "
                             "20 minutes before returning",
                        stack_pos=2, stack=pd_enders_game, author=david)
 
-    nw_11_pd = Writing(text="Ender's Game",
+    nw_11_pd = Writing(id_=32, text="Ender's Game",
                        stack_pos=0, stack=pd_enders_game, author=nathan)
 
-    ew_11_pd = Writing(text="An Awful Eiffel Tower",
+    ew_11_pd = Writing(id_=33, text="An Awful Eiffel Tower",
                        stack_pos=0, stack=pd_eiffel_tower, author=elwood)
-    ew_12_pd = Writing(text="Passing an hour on the fourth Monday.",
+    ew_12_pd = Writing(id_=34, text="Passing an hour on the fourth Monday.",
                        stack_pos=2, stack=pd_yesterday, author=elwood)
 
-    dd_11_pd = Drawing(drawing=open(os.path.join('data', 'drawings', 'dd_11.jpg'), 'rb').read(),
+    dd_11_pd = Drawing(id_=26, drawing=open(os.path.join('data', 'drawings', 'dd_11.jpg'), 'rb').read(),
                        stack_pos=1, stack=pd_eiffel_tower, author=david)
 
-    nd_11_pd = Drawing(drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
+    nd_11_pd = Drawing(id_=27, drawing=open(os.path.join('data', 'drawings', 'nd_11.jpg'), 'rb').read(),
                        stack_pos=1, stack=pd_yesterday, author=nathan)
 
-    ed_11_pd = Drawing(drawing=open(os.path.join('data', 'drawings', 'ed_11.jpg'), 'rb').read(),
+    ed_11_pd = Drawing(id_=28, drawing=open(os.path.join('data', 'drawings', 'ed_11.jpg'), 'rb').read(),
                        stack_pos=1, stack=pd_enders_game, author=elwood)
 
     with d.session_scope() as session:
