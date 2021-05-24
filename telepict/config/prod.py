780a192b34cac3a68785b38c17077ec73f4409f2
David Pitchford <david.t.pitchford@gmail.com>
2021-05-24 14:44:50 -0500

    Set dbfile in prof

--- telepict/config/prod.py
+++ telepict/config/prod.py
@@ -5,6 +5,8 @@ from .base import Config, make_logging_config
 from ..util.image.s3_based import S3ImageBackend
 
 class ConfigProd(Config):
+    DBFILE = os.path.expanduser(os.path.join('~', '.telepict', 'telepict.db'))
+    DB_URL = f"sqlite:///{DBFILE}"
     ACCESS_CODE_FILE = '/tmp/telepict_access_code'
     REQUIRE_ACCESS_CODE = True
     SECRET_KEY_FILE = '/home/ec2-user/.secret_key'
