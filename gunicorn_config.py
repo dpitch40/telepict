6ab2690be15b243ac91d6069942a7977c93da1dc
David Pitchford <david.t.pitchford@gmail.com>
2021-05-28 10:37:20 -0500

    Set up HTTPS

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,6 +1,8 @@
 import multiprocessing
 
-bind = "0.0.0.0:80"
+bind = "0.0.0.0:443"
 workers = multiprocessing.cpu_count() * 2 + 1
 accesslog = '/var/log/telepict/gunicorn-access.log'
 errorlog = '/var/log/telepict/gunicorn-error.log'
+keyfile = '/etc/letsencrypt/live/www.telephone-pictionary.net/privkey.pem'
+certfile = '/etc/letsencrypt/live/www.telephone-pictionary.net/fullchain.pem'
